5c9ab0b28a1e58c86a602f3920f46c7d2118a52d
Maikel Traksel <maikel_t14@hotmail.com>
2015-11-05 23:53:12 +0100

    thumbnail fix in show. made a start in getting the content in index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @project = Project.all
   end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,8 +33,13 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [50, 50]
+    process resize_to_fit: [360, 257]
   end
+
+  version :smal_thumb do
+    process resize_to_fit: [100, 100]
+  end
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
